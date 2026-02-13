33ed3e44a06188d46abe1c8db71574ab36db013d
Bhushan Daddikar <bhushan.daddikar@otsi.co.in>
2026-02-13 21:21:30 +0530

    Time out for 5 mins

--- app/api/process-video/route.ts
+++ app/api/process-video/route.ts
@@ -4,6 +4,8 @@ import { eq } from "drizzle-orm";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+const LAMBDA_TIMEOUT_MS = 5 * 60 * 1000;
+
 async function runProcessingJob({
   resultId,
   videoUri,
@@ -28,14 +30,22 @@ async function runProcessingJob({
     params.append("presigned_expiry_seconds", "");
     params.append("result_id", resultId);
 
-    const response = await fetch(process.env.LAMBDA_ENDPOINT!, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: params.toString(),
-    });
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), LAMBDA_TIMEOUT_MS);
+    let response: Response;
+    try {
+      response = await fetch(process.env.LAMBDA_ENDPOINT!, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: params.toString(),
+        signal: controller.signal,
+      });
+    } finally {
+      clearTimeout(timeout);
+    }
 
     const responseText = await response.text();
     let lambdaData: unknown;
