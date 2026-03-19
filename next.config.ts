4d5b352b1818bf73ef30223d8dcafd6426e6db37
Kushal C <kushal.c@otsi.co.in>
2026-03-19 11:49:42 +0530

    nextjs-congig

--- next.config.ts
+++ next.config.ts
@@ -6,5 +6,22 @@ const nextConfig: NextConfig = {
     // middlewareClientMaxBodySize: "50mb",
     proxyClientMaxBodySize: "50mb",
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "", // Remove SAMEORIGIN restriction
+          },
+        ],
+      },
+    ];
+  },
 };
 export default nextConfig;
