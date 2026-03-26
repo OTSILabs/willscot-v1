a3c5826d77c7376fb00a577e2dffb02d3e3236b5
Kushal C <kushal.c@otsi.co.in>
2026-03-26 19:17:21 +0530

    temp-folder-delete

--- tmp/list-users.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "./lib/db";
-import { users } from "./lib/db/schema";
-
-async function main() {
-  const allUsers = await db.select().from(users).limit(10);
-  console.log(JSON.stringify(allUsers, null, 2));
-}
-
-main().catch(console.error);
